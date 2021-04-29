1a51fca97e1578b74ea995e87cce936a611b7f2a
danieleguzmans <daniel.e.guzman.s@gmail.com>
2021-04-28 20:01:34 -0400

    primer commit de la aplicacion strapi server

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9393394439576848ae025bc7e2285e27'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e47749fd-6279-4cd4-9b80-70f8e7acaae8'
+};
